ea845d5ba43e889475747c346a5c3c6ffe1a6a34
sakhan8-radar <57330603+sakhan8-radar@users.noreply.github.com>
2019-11-26 16:22:13 -0500

    Update sound_output_test_file.m

--- sound_output_test_file.m
+++ sound_output_test_file.m
@@ -1,37 +1,34 @@
-% raw_data = rand(10, 2) + i*(rand(10,2))
-% 
-% % raw_data = 1 + 1*i;
-% 
-% angle_degree1 = (angle(raw_data(1)))*(180/pi)
-% angle_degree2 = (angle(raw_data(2)))*(180/pi)
-
+%%%%% Sound output test file with self-generated raw data output.
+    % ydata - 10 by 2 matrix of self-generated raw data
+    % target - 10 by 1 matrix with target angle calculted by subtracting
+    % the angle from the first angle with teh angle from the second column
+% Audio data - customize audio file used and find file type
 syms y Fs
-[y,Fs] = audioread('FoolInTheRain.wav')
+[y,Fs] = audioread('Fool_In_The_Rain.wav')
 player = audioplayer(y,Fs);
-% sound(y,Fs)
-% clear sound
-i = 1;
-while i == 1
-target = 1
-if target == 1
-    play(player);
-elseif target == 0
-    pause(player);
+info = audioinfo('Fool_In_The_Rain.wav')
+
+% Simulating matrix to act as raw data from radar. Takes information on radar angle.
+target = zeros(10,1)
+% ydata = zeros(10,2) % cuts off sound to test whether "raw data" detects a target
+ydata = rand(10,2) % allows for sound output
+for i = 1:1:10
+    if mod(i,2) == 1
+    ydata(i,1) = rand + 1i*rand; % filling first column of "raw data" with complex #s in order to simulate a target angle
+    end
+    target(i,1) = angle(ydata(i,1)) - angle(ydata(i,2));
+    if target(i) > 0
+        play(player); % Refers to chosen audio file at beginning of code
+        % insert code for Karplus Strong Algorithm % another option is to
+        % use the K-S algorithm rather than an audio file
+        pause(10) % wait 10 seconds before moving on to the next line
+    else
+        stop(player); % Refers to chosen audio file at beginning of code
+        pause(10) % wait 10 seconds before moving on to the next line
+    end
 end
-end
-stop(player);
+disp(ydata)
+disp(target*(180/pi))
+% stop(player)
 
 
-
-% % while loop
-% target = 1; % target is either in or out. When target is in, target = 1. When target is out, target = 0.
-% 
-% when target == 1
-
-% info = audioinfo('FoolInTheRain.mp3')
-% audiowrite('FoolInTheRain.wav',y,Fs)
-% clear y Fs
-
-% elseif target == 0 pause music
-% while loop end
-% Do a continuous run through to process the target as in or out
